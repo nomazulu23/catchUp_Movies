7bf057f6f33f3f4776e0395fa653eced7dbc47f4
Heather Jasi <u033813021@byui.edu>
2023-05-18 13:17:22 +0200

    Initial Commit for Final Project

---
+++ catchUp.js
@@ -0,0 +1,60 @@
+const apiKey = '853b19f1916650afe0cb18602242a747'; // Replace with your TMDb API key
+const apiUrl = 'https://api.themoviedb.org/3';
+
+// Function to fetch and display movies
+async function fetchMovies(searchQuery = '') {
+  let endpoint = '/movie/popular';
+  
+  if (searchQuery) {
+    endpoint = '/search/movie';
+    searchQuery = encodeURIComponent(searchQuery);
+  }
+  
+
+  const response = await fetch(`${apiUrl}${endpoint}?api_key=${apiKey}&query=${searchQuery}`);
+  const data = await response.json();
+
+  const moviesContainer = document.getElementById('movies');
+  moviesContainer.innerHTML = '';
+
+  if (data.results.length === 0) {
+    moviesContainer.innerHTML = '<p>No movies found.</p>';
+    return;
+  }
+
+  data.results.forEach(movie => {
+    const movieElement = document.createElement('div');
+    movieElement.innerHTML = `
+      <h2>${movie.title}</h2>
+      <p>${movie.overview}</p>
+      <button onclick="showMovieDetails(${movie.id})">Details</button>
+    `;
+    moviesContainer.appendChild(movieElement);
+  });
+}
+
+// Function to show movie details
+async function showMovieDetails(movieId) {
+  const response = await fetch(`${apiUrl}/movie/${movieId}?api_key=${apiKey}`);
+  const movie = await response.json();
+
+  const movieDetailsContainer = document.getElementById('movieDetails');
+  movieDetailsContainer.innerHTML = `
+    <h2>${movie.title}</h2>
+    <p>${movie.overview}</p>
+    <p>Release Date: ${movie.release_date}</p>
+    <p>Rating: ${movie.vote_average}</p>
+  `;
+}
+
+// Function to handle form submission
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const searchInput = document.getElementById('searchInput');
+  const searchQuery = searchInput.value;
+  fetchMovies(searchQuery);
+  searchInput.value = '';
+});
+
+// Initial fetch to display popular movies
+fetchMovies();
